13883aa9b065629ad82ab40ef5920935e4be6119
Brayan Mendez <bmendezchay@gmail.com>
2024-05-30 20:43:01 -0600

    calculadora

---
+++ assets/readme.md
@@ -0,0 +1,4 @@
+## Esta es una calculadora 
+## con HTML, JavaScript, CSS y puede cambiar de tema con un boton esta echo con eventos click tambien tiene un cambio de tema con un boton   
+![alt text](./img/image.png)
+![alt text](./img/image2.png)
---
+++ calculadora.js
@@ -0,0 +1,149 @@
+const btnCambiar = document.querySelector('#btn-cambiar')
+const cambio = document.querySelector('#cambio')
+const calculadora2 = document.querySelector('#calculadora2')
+const b7 = document.querySelector('#b7')
+const b8 = document.querySelector('#b8')
+const b9 = document.querySelector('#b9')
+const b4 = document.querySelector('#b4')
+const b5 = document.querySelector('#b5')
+const b6 = document.querySelector('#b6')
+const b1 = document.querySelector('#b1')
+const b2 = document.querySelector('#b2')
+const b3 = document.querySelector('#b3')
+const b0 = document.querySelector('#b0')
+const btnColor0 = document.querySelector('#btn-color0')
+const btnColor1 = document.querySelector('#btn-color1')
+const btnColor2 = document.querySelector('#btn-color2')
+const btnColor3 = document.querySelector('#btn-color3')
+const btnColor4 = document.querySelector('#btn-color4')
+const colorDelete = document.querySelector('#color-delete')
+const colorC = document.querySelector('#color-c')
+const colorPunto = document.querySelector('#color-punto')
+
+
+
+btnCambiar.addEventListener('click', () => {
+    cambio.classList.toggle('contenedor',)
+    cambio.classList.toggle('tema2-contenedor')
+    calculadora2.classList.toggle('calculadora')
+    calculadora2.classList.toggle('calculadora2')
+    texto.classList.toggle('texto1')
+    texto.classList.toggle('texto2')
+    b7.classList.toggle('boton1')
+    b7.classList.toggle('boton2')
+    b8.classList.toggle('boton1')
+    b8.classList.toggle('boton2')
+    b9.classList.toggle('boton1')
+    b9.classList.toggle('boton2')
+    b4.classList.toggle('boton1')
+    b4.classList.toggle('boton2')
+    b5.classList.toggle('boton1')
+    b5.classList.toggle('boton2')
+    b6.classList.toggle('boton1')
+    b6.classList.toggle('boton2')
+    b1.classList.toggle('boton1')
+    b1.classList.toggle('boton2')
+    b2.classList.toggle('boton1')
+    b2.classList.toggle('boton2')
+    b3.classList.toggle('boton1')
+    b3.classList.toggle('boton2')
+    b0.classList.toggle('boton1')
+    b0.classList.toggle('boton2')
+    btnColor0.classList.toggle('operadores')
+    btnColor0.classList.toggle('operadores2')
+    btnColor1.classList.toggle('operadores')
+    btnColor1.classList.toggle('operadores2')
+    btnColor2.classList.toggle('operadores')
+    btnColor2.classList.toggle('operadores2')
+    btnColor3.classList.toggle('operadores')
+    btnColor3.classList.toggle('operadores2')
+    btnColor4.classList.toggle('operadores')
+    btnColor4.classList.toggle('operadores2')
+    colorDelete.classList.toggle('delete')
+    colorDelete.classList.toggle('delete2')
+    colorC.classList.toggle('btn-C')
+    colorC.classList.toggle('btn-C2')
+    colorPunto.classList.toggle('operadores')
+    colorPunto.classList.toggle('operadores2')
+
+})
+// boton de igual da el resultado de la operacion
+
+// operadores 
+
+// selecion de todas las clases
+const btnC = document.querySelector('.btn-C')
+const texto = document.querySelector('#texto')
+let numeros = document.querySelectorAll('.numero');
+let btnAccion = document.querySelectorAll('.accion')
+let primerNumero = 0;
+let segundoNumero = 0;
+let acumulador = 0;
+let resultadoResta = 0;
+let resultadoDivision = 0;
+let resultadoMultiplicar = 0;
+let signo = '';
+
+btnC.addEventListener('click', () => {
+    texto.value = ''
+    segundoNumero = 0;
+    primerNumero = 0;
+    acumulador = 0;
+    signo = ''
+})
+
+// es un bucle que permite recorrer estructuras que contienen varios elementos que serian 10
+
+numeros.forEach((btn) => {
+
+    btn.addEventListener('click', () => {
+        texto.value += btn.value
+
+    });
+});
+
+btnAccion.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+
+
+        if (e.target.value == '+' || e.target.value == '-' ||
+            e.target.value == '/' || e.target.value == '*') {
+            primerNumero = parseInt(texto.value)
+            acumulador += primerNumero
+            texto.value = ''
+            signo = e.target.value
+
+        }
+
+        else if (e.target.value == '=') {
+
+
+            if (signo == '+') {
+                segundoNumero = parseInt(texto.value)
+                resultado = acumulador + segundoNumero
+                texto.value = resultado
+            }
+            else if (signo == '-') {
+                segundoNumero = parseInt(texto.value)
+                let resultado = primerNumero - segundoNumero
+                texto.value = resultado
+
+            } else if (signo == '/') {
+                segundoNumero = parseInt(texto.value)
+                let resultado = acumulador / segundoNumero
+                texto.value = resultado
+
+            } else if (signo == '*') {
+                segundoNumero = parseInt(texto.value)
+                let resultado = acumulador * segundoNumero
+                texto.value = resultado
+            }
+            acumulador = 0;
+        }
+
+
+
+
+    })
+})
+
