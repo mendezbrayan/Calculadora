88e703b865dfb2fba4b53c53c8234d2c08f7661f
Brayan Mendez <bmendezchay@gmail.com>
2024-06-04 17:50:20 -0600

    calculadora terminada

--- calculadora.js
+++ calculadora.js
@@ -1,21 +1,8 @@
 const btnCambiar = document.querySelector('#btn-cambiar')
 const cambio = document.querySelector('#cambio')
 const calculadora2 = document.querySelector('#calculadora2')
-const b7 = document.querySelector('#b7')
-const b8 = document.querySelector('#b8')
-const b9 = document.querySelector('#b9')
-const b4 = document.querySelector('#b4')
-const b5 = document.querySelector('#b5')
-const b6 = document.querySelector('#b6')
-const b1 = document.querySelector('#b1')
-const b2 = document.querySelector('#b2')
-const b3 = document.querySelector('#b3')
-const b0 = document.querySelector('#b0')
-const btnColor0 = document.querySelector('#btn-color0')
-const btnColor1 = document.querySelector('#btn-color1')
-const btnColor2 = document.querySelector('#btn-color2')
-const btnColor3 = document.querySelector('#btn-color3')
-const btnColor4 = document.querySelector('#btn-color4')
+const boton1 = document.querySelectorAll('.boton1')
+const operadores = document.querySelectorAll('.operadores')
 const colorDelete = document.querySelector('#color-delete')
 const colorC = document.querySelector('#color-c')
 const colorPunto = document.querySelector('#color-punto')
@@ -23,172 +10,180 @@ const colorPunto = document.querySelector('#color-punto')
 
 
 btnCambiar.addEventListener('click', () => {
-    cambio.classList.toggle('contenedor',)
+
+    cambio.classList.toggle('contenedor')
     cambio.classList.toggle('tema2-contenedor')
     calculadora2.classList.toggle('calculadora')
     calculadora2.classList.toggle('calculadora2')
     texto.classList.toggle('texto1')
     texto.classList.toggle('texto2')
-    b7.classList.toggle('boton1')
-    b7.classList.toggle('boton2')
-    b8.classList.toggle('boton1')
-    b8.classList.toggle('boton2')
-    b9.classList.toggle('boton1')
-    b9.classList.toggle('boton2')
-    b4.classList.toggle('boton1')
-    b4.classList.toggle('boton2')
-    b5.classList.toggle('boton1')
-    b5.classList.toggle('boton2')
-    b6.classList.toggle('boton1')
-    b6.classList.toggle('boton2')
-    b1.classList.toggle('boton1')
-    b1.classList.toggle('boton2')
-    b2.classList.toggle('boton1')
-    b2.classList.toggle('boton2')
-    b3.classList.toggle('boton1')
-    b3.classList.toggle('boton2')
-    b0.classList.toggle('boton1')
-    b0.classList.toggle('boton2')
-    btnColor0.classList.toggle('operadores')
-    btnColor0.classList.toggle('operadores2')
-    btnColor1.classList.toggle('operadores')
-    btnColor1.classList.toggle('operadores2')
-    btnColor2.classList.toggle('operadores')
-    btnColor2.classList.toggle('operadores2')
-    btnColor3.classList.toggle('operadores')
-    btnColor3.classList.toggle('operadores2')
-    btnColor4.classList.toggle('operadores')
-    btnColor4.classList.toggle('operadores2')
-    colorDelete.classList.toggle('delete')
-    colorDelete.classList.toggle('delete2')
     colorC.classList.toggle('btn-C')
     colorC.classList.toggle('btn-C2')
-    colorPunto.classList.toggle('operadores')
-    colorPunto.classList.toggle('operadores2')
+    colorDelete.classList.toggle('delete')
+    colorDelete.classList.toggle('delete2')
 
+    boton1.forEach((btn) => {
+        btn.classList.toggle('boton1')
+        btn.classList.toggle('boton2')
+    })
+    operadores.forEach((btn) => {
+        btn.classList.toggle('operadores')
+        btn.classList.toggle('operadores2')
+    })
 })
-// boton de igual da el resultado de la operacion
 
-// operadores 
-
-// selecion de todas las clases
 const btnC = document.querySelector('.btn-C')
 const texto = document.querySelector('#texto')
 let numeros = document.querySelectorAll('.numero');
 let btnAccion = document.querySelectorAll('.accion')
 let primerNumero = 0;
-let numeroMult;
 let segundoNumero = 0;
 let acumulador = 0;
-let opera;
-let resultado;
-let resultado3 = 1;
 let signo = '';
 let primeraVez = true;
-let re = 0;
 
 
-btnC.addEventListener('click', () => {
-    texto.value = '';
-    segundoNumero = 0;
-    primerNumero = 0;
-    acumulador = 0;
-    resultado = 0;
-     signo = ''
-})
 
-// es un bucle que permite recorrer estructuras que contienen varios elementos que serian 10
+
+
 
 numeros.forEach((btn) => {
 
     btn.addEventListener('click', () => {
         texto.value += btn.value
-        console.log(btn.value)
 
     });
 });
+const suma = () => {
+    acumulador += primerNumero
+
+}
+const resta = () => {
+    if (primeraVez) {
+        acumulador = primerNumero + Math.abs(acumulador)
+        primeraVez = false;
+    } else if (acumulador >= 0) {
+        acumulador = Math.abs(acumulador) - primerNumero
+    } else {
+        acumulador = acumulador - primerNumero
+    }
+
+}
+const multiplicacion = () => {
+    if(acumulador == 0){
+        acumulador = 1;
+        acumulador = acumulador * primerNumero
+    }else{
+        acumulador * primerNumero
+    }
+
+}
+const division = () => {
+    if (acumulador == 0) {
+        acumulador = primerNumero 
+    } else {
+        acumulador = acumulador / primerNumero
+    }
+
+}
+const igual = () => {
+
+    if (signo == '+') {
+        acumulador = 0;
+        segundoNumero = parseInt(texto.value)
+        acumulador = acumulador + segundoNumero
+        texto.value = acumulador
+
+    }
+    else if (signo == '-') {
+
+        segundoNumero = parseInt(texto.value)
+         acumulador = acumulador - segundoNumero
+        texto.value = acumulador
+        primeraVez = true;
+
+    } else if (signo == '/') {
+
+        segundoNumero = parseInt(texto.value)
+         acumulador = acumulador / segundoNumero
+        texto.value = acumulador
+        
+
+    } else if (signo == '*') {
+    segundoNumero = parseInt(texto.value)
+        
+         acumulador = acumulador * segundoNumero
+        texto.value = acumulador
+        
+
+    }
+
+}
 
 btnAccion.forEach((btn) => {
     btn.addEventListener('click', (e) => {
 
-        if (e.target.value == '+' || e.target.value == '-' ||
-            e.target.value == '/' || e.target.value == '*') {
-                
-                primerNumero = parseInt(texto.value)
+        if(texto.value == ''){
+            alert('Ingrese un Numero')
+        }
+       else if (e.target.value != '=') {
+
+            primerNumero = parseInt(texto.value)
             switch (e.target.value) {
                 case '+':
-                    acumulador += primerNumero
+                    if (signo != '')// el signo debe ser diferente a vacio para que entre en el if
+             
+                     {
+                        igual()
+                    } else {
+                        suma()
+                    }
                     break;
                 case '-':
-                    if (primeraVez) {
-                        acumulador = primeraVez + primerNumero
-                        primeraVez = false;
-                    } else if (acumulador >= 0) {
-                        acumulador = Math.abs(acumulador) - primerNumero
+                    if (signo != '') {
+                        igual()
                     } else {
-                        acumulador = acumulador - Math.absprimerNumero
+                        resta()
                     }
                     break;
                 case '*':
-
-                    resultado3 = resultado3 * primerNumero
-
-
-                    break;
-                case '/':
-                    if(primeraVez){
-                        acumulador = primerNumero + acumulador
-                        primeraVez = false
-                    }else{
-                        acumulador = acumulador / primerNumero
+                    if (signo != '') {
+                        igual()
+                    } else {
+                        multiplicacion()
                     }
 
                     break;
+                case '/':
+                    if (signo != '') {
+                        igual()
+                    } else {
+                        division()
+                    }
+                    break;
 
 
             }
 
-            texto.value = ''
-            signo = e.target.value
+            texto.value = '' 
+            signo = e.target.value 
 
-        }
-
-        else if (e.target.value == '=') {
-
-
-            if (signo == '+') {
-                segundoNumero = parseInt(texto.value)
-                resultado = acumulador + segundoNumero
-                texto.value = resultado
-            }
-            else if (signo == '-') {
-                
-                segundoNumero = parseInt(texto.value)
-                let resultado = acumulador - segundoNumero
-                texto.value = resultado
-                
-
-            } else if (signo == '/') {
-                segundoNumero = parseInt(texto.value)
-                resultado = acumulador / segundoNumero
-                texto.value = resultado
-                primeraVez = true
-
-            } else if (signo == '*') {
-                
-                segundoNumero = parseInt(texto.value)
-                let resultado = resultado3 * segundoNumero
-                
-                texto.value = resultado
-            }
-            acumulador = 0;
-            resultado3 = 1;
-
-        }
-
-
+        }else {
+            
+         igual()
+         signo = '';
+         acumulador = 0;
+    }
 
 
     })
 })
+btnC.addEventListener('click', () => {
+    texto.value = '';
+    segundoNumero = 0;
+    primerNumero = 0;
+    acumulador = 0;
+    primeraVez = true;
+    signo = ''
 
+})
--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
 ## Esta es una calculadora 
-## con HTML, JavaScript, CSS , puede cambiar de tema con un boton, esta echo con eventos click
-## puede sumar,restar,dividr y multiplicar  
-![alt text](./assets/img/image.png)
-![alt text](./assets/img/image2.png)
+## con HTML, JavaScript, CSS , puede cambiar de tema con un boton, esta echo con eventos click 
+## Puede sumar, restar, dividir y multiplicar  
+![alt text](./assets/img/image-3.png)
+![alt text](./assets/img/image-4.png)
